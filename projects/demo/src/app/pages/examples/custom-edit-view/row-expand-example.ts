e9f94c0fc89fa83d038510c9ee66ed9f0ae6b5e3
Mike Becker <universe@uap-core.de>
2023-07-14 11:54:42 +0200

    make row-expand-example.ts more reasonable

--- projects/demo/src/app/pages/examples/custom-edit-view/row-expand-example.ts
+++ projects/demo/src/app/pages/examples/custom-edit-view/row-expand-example.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Settings} from "angular2-smart-table";
 
 @Component({
@@ -49,6 +49,7 @@ export class RowExpandComponent implements OnInit {
 
   settings: Settings = {
     resizable: true,
+    hideable: true,
     expand: {
       component: DivViewComponent,
     },
